078b6063253875d420a24ab8f0b808d97e5d8ea5
Pascal Van Cleeff <pascal.van.cleeff@gmail.com>
2020-04-23 21:58:19 +0200

    Add hex game

---
+++ hex/precache-manifest.161812e85f1683c8f7c993379cd9790d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13139cc29d0fc60d7e20057342457fec",
+    "url": "/hex/index.html"
+  },
+  {
+    "revision": "5068b802b15b016453a5",
+    "url": "/hex/static/css/main.1f91eda2.chunk.css"
+  },
+  {
+    "revision": "ee595634f7ab517f1e22",
+    "url": "/hex/static/js/2.04a353b6.chunk.js"
+  },
+  {
+    "revision": "1c362552069d7b44ce49fcb08cee10da",
+    "url": "/hex/static/js/2.04a353b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5068b802b15b016453a5",
+    "url": "/hex/static/js/main.52ecbb5c.chunk.js"
+  },
+  {
+    "revision": "6605f9906f94a08e11e0",
+    "url": "/hex/static/js/runtime-main.cc4f3486.js"
+  }
+]);
